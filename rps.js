6b47278f99139bd8cf28f8e6c2c889869e1de309
Caitlin-2006 <caitlin.ohare@ada.ac.uk>
2024-09-25 13:51:14 +0000

    Refactored solution using dictionary

--- rps.js
+++ rps.js
@@ -1,25 +1,20 @@
 function rockPaperScissors(player1, player2) {
+  const conditionsToWin = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"],
+  };
+  
   if (player1 === player2) {
     return "draw";
-  } else if (
-    (player1 === "rock" && player2 === "scissors") ||
-    (player1 === "scissors" && player2 === "paper") ||
-    (player1 === "paper" && player2 === "rock") ||
-    (player1 === "lizard" && player2 === "spock") ||
-    (player1 === "spock" && player2 === "scissors") ||
-    (player1 === "rock" && player2 === "lizard") ||
-    (player1 === "paper" && player2 === "spock") ||
-    (player1 === "scissors" && player2 === "lizard") ||
-    (player1 === "lizard" && player2 === "paper") ||
-    (player1 === "spock" && player2 === "rock")
-  ) {
-    return "player1";
-  } else {
-    return "player2";
+  };
+  
+  return conditionsToWin[player1].includes(player2) ? "player1" : "player2";
   }
-}
-
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-};
+  };
+
