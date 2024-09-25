dad255d41fc17662329400a6eeb7db30c1705d25
Caitlin-2006 <caitlin.ohare@ada.ac.uk>
2024-09-25 14:45:42 +0000

    formatted and spacing

--- rps.js
+++ rps.js
@@ -6,13 +6,12 @@ function rockPaperScissors(player1, player2) {
     lizard: ["spock", "paper"],
     spock: ["scissors", "rock"],
   };
-  
   if (player1 === player2) {
     return "draw";
   };
-  
   return conditionsToWin[player1].includes(player2) ? "player1" : "player2";
   }
+  
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
