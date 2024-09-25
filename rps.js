29aff396d913fdfc0d09a6ae823244e06528c715
Caitlin-2006 <caitlin.ohare@ada.ac.uk>
2024-09-25 09:45:20 +0000

    pass tests for draws

--- rps.js
+++ rps.js
@@ -1,19 +1,12 @@
 function rockPaperScissors(player1, player2) {
-
-  
-   
+  if (player1 === player2) {
+    return "draw";
+  }
 }
-
-
-
-
-
-
-
 
 
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+};
