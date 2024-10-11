226c6ff24f889889236868c6dba7d81ab9f69107
Caitlin-2006 <caitlin.ohare@ada.ac.uk>
2024-10-11 09:27:02 +0000

    Final version

--- rps.js
+++ rps.js
@@ -6,14 +6,11 @@ function rockPaperScissors(player1, player2) {
     lizard: ["spock", "paper"],
     spock: ["scissors", "rock"],
   };
-  if (player1 === player2) {
-    return "draw";
-  };
+  if (player1 === player2) return "draw";
   return conditionsToWin[player1].includes(player2) ? "player1" : "player2";
-  }
-  
+}
+
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-  };
-
+};
